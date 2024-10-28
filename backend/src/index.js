21f4f5f7dd8101371e920421f8a641839a39b504
agvidit1 <agvidit1@umd.edu>
2024-10-28 19:41:35 -0400

    Fixing up the application.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
 
 mongoose.connect('mongodb://mongo:27017/appointments', { 
     useNewUrlParser: true, 
