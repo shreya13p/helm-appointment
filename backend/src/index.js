002c02115a3614e94c5afae1985fb7390160f690
agvidit1 <agvidit1@umd.edu>
2024-10-28 20:48:36 -0400

    Resolving minor error.

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'build')));
 
 mongoose.connect('mongodb://mongo:27017/appointments', { 
     useNewUrlParser: true, 
